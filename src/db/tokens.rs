83c795c557ac2c5305bd379aa254d63eae45ca3a
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-03-12 12:40:56 -0700

    Add Tokens methods: create, authenticate, destroy
    Still need list, but that needs an extra helper.

---
+++ src/db/tokens.rs
@@ -0,0 +1,199 @@
+use super::users::User;
+use crate::util::{sha256sum, uuid_string};
+use anyhow::anyhow;
+use sqlx::{query, query_as, SqlitePool};
+use time::OffsetDateTime;
+
+/// A query helper type for operating on [Token]s. Usually rented from a Db.
+pub struct Tokens<'a> {
+    pool: &'a SqlitePool,
+}
+
+/// Record struct for API authentication tokens associated with a [User].
+/// Unlike User passwords, tokens can't be chosen by a user or re-used
+/// elsewhere, so they don't need a time-wasting hash function like bcrypt
+/// or argon2. We still don't store the token cleartext itself, but we
+/// just hash it with plain old unsalted sha256. Sometimes the classics
+/// are best.
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub id: i64,
+    pub user_id: i64,
+    scope: String, // private, use .scope().
+    pub created: OffsetDateTime,
+    pub last_used: Option<OffsetDateTime>,
+    pub comment: Option<String>,
+    // notably excluded: token_hash and also the temporary cleartext.
+}
+
+impl Token {
+    pub fn scope(&self) -> TokenScope {
+        self.scope.as_str().into()
+    }
+}
+
+impl PartialEq for Token {
+    fn eq(&self, other: &Self) -> bool {
+        // Skip last_used bc it can change on auth-fetch
+        self.id == other.id
+            && self.user_id == other.user_id
+            && self.scope == other.scope
+            && self.created == other.created
+            && self.comment == other.comment
+    }
+}
+
+/// The exhaustive list of full permission types that API tokens can have.
+/// These values are stored in the database as text, but the application
+/// code can have a little enum. as a treat.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub enum TokenScope {
+    /// Text: `write_dogears`.
+    /// Can POST `/api/v1/create` and `/api/v1/update`.
+    WriteDogears,
+    /// Text: `manage_dogears`.
+    /// Can POST `/api/v1/create` and `/api/v1/update`.
+    /// Can GET `/api/v1/list`.
+    /// Can DELETE `/api/v1/dogear/:id`.
+    ManageDogears,
+    /// Can't do shit!!
+    Invalid,
+}
+
+impl From<&str> for TokenScope {
+    fn from(value: &str) -> Self {
+        match value {
+            "write_dogears" => Self::WriteDogears,
+            "manage_dogears" => Self::ManageDogears,
+            _ => Self::Invalid,
+        }
+    }
+}
+
+impl From<TokenScope> for &'static str {
+    fn from(value: TokenScope) -> Self {
+        match value {
+            TokenScope::WriteDogears => "write_dogears",
+            TokenScope::ManageDogears => "manage_dogears",
+            TokenScope::Invalid => "INVALID",
+        }
+    }
+}
+
+impl<'a> Tokens<'a> {
+    /// Create a token, and return it along with the *actual token cleartext.*
+    /// This is the only time the cleartext is ever available.
+    pub async fn create(
+        &self,
+        user_id: i64,
+        scope: TokenScope,
+        comment: Option<&str>,
+    ) -> anyhow::Result<(Token, String)> {
+        let token_cleartext = uuid_string();
+        let token_hash = sha256sum(&token_cleartext);
+        let scope_str: &str = scope.into();
+        let token = query_as!(
+            Token,
+            r#"
+                INSERT INTO tokens (user_id, token_hash, scope, comment)
+                VALUES (?1, ?2, ?3, ?4)
+                RETURNING id, user_id, scope, created, last_used, comment;
+            "#,
+            user_id,
+            token_hash,
+            scope_str,
+            comment
+        )
+        .fetch_one(self.pool)
+        .await?;
+
+        Ok((token, token_cleartext))
+    }
+
+    /// Use the provided token cleartext to look up a token and its associated user.
+    /// Returns Ok(None) if the token doesn't match anything.
+    pub async fn authenticate(
+        &self,
+        token_cleartext: &str,
+    ) -> anyhow::Result<Option<(Token, User)>> {
+        let token_hash = sha256sum(token_cleartext);
+
+        // First off, we do a fire-and-forget last-used bump. If this update
+        // whiffs, that's fine.
+        //
+        // (I wanted to do everything in one go, but I need the user too, and
+        // sqlite can't access an update's FROM clause in its RETURNING clause.)
+        query!(
+            r#"
+                UPDATE tokens
+                SET last_used = CURRENT_TIMESTAMP
+                WHERE token_hash = ?;
+            "#,
+            token_hash
+        )
+        .execute(self.pool)
+        .await?;
+
+        // Use query!() instead of query_as!(), because we want multiple records
+        // and we don't have a struct for "user plus token fields".
+        let maybe = query!(
+            r#"
+                SELECT
+                    tokens.id        AS token_id,
+                    tokens.user_id   AS user_id,
+                    tokens.scope     AS token_scope,
+                    tokens.created   AS token_created,
+                    tokens.last_used AS token_last_used,
+                    tokens.comment   AS token_comment,
+                    users.username   AS user_username,
+                    users.email      AS user_email,
+                    users.created    AS user_created
+                FROM tokens JOIN users ON tokens.user_id = users.id
+                WHERE tokens.token_hash = ? LIMIT 1;
+            "#,
+            token_hash
+        )
+        .fetch_optional(self.pool)
+        .await?;
+
+        // Early out if we got nuthin
+        let Some(stuff) = maybe else {
+            return Ok(None);
+        };
+        let token = Token {
+            id: stuff.token_id,
+            user_id: stuff.user_id,
+            scope: stuff.token_scope,
+            created: stuff.token_created,
+            last_used: stuff.token_last_used,
+            comment: stuff.token_comment,
+        };
+        let user = User {
+            id: stuff.user_id,
+            username: stuff.user_username,
+            email: stuff.user_email,
+            created: stuff.user_created,
+        };
+        Ok(Some((token, user)))
+    }
+
+    /// Delete a token. To double-check the permissions, get the token's
+    /// user ID from a trusted source and provide it when calling this.
+    pub async fn destroy(&self, id: i64, user_id: i64) -> anyhow::Result<()> {
+        let res = query!(
+            r#"
+                DELETE FROM tokens
+                WHERE id = ?1 AND user_id = ?2;
+            "#,
+            id,
+            user_id,
+        )
+        .execute(self.pool)
+        .await?;
+        if res.rows_affected() != 1 {
+            Err(anyhow!("Couldn't find the specified token."))
+        } else {
+            Ok(())
+        }
+    }
+}
