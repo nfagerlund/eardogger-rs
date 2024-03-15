c1b77bfd73acb56d738f6b9001d4e4885aa030ae
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-03-15 10:14:10 -0700

    Migrations: Add dogears table!
    Differences from eardogger 1: NOT NULL constraints on `current` and `updated`.

---
+++ migrations/20240315170914_create-dogears-table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS dogears(
+    id INTEGER PRIMARY KEY NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    prefix TEXT NOT NULL,
+    current TEXT NOT NULL,
+    display_name TEXT,
+    updated TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
