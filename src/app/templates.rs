9e41ae702b23538ae0b25d7623805a7dd489387c
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-03-18 13:19:21 -0700

    Statically load all the templates..... brutal.

--- src/app/templates.rs
+++ src/app/templates.rs
@@ -57,3 +57,55 @@ struct ErrorPage<'a> {
 }
 
 // TODO still: bookmarklets.
+
+// For now, I'm just gonna load all the templates statically and compile em
+// in to the app.
+pub fn load_templates() -> anyhow::Result<minijinja::Environment<'static>> {
+    let mut env = minijinja::Environment::new();
+    env.add_template(
+        "_layout.html.j2",
+        include_str!("../../templates/_layout.html.j2"),
+    )?;
+    env.add_template(
+        "account.html.j2",
+        include_str!("../../templates/account.html.j2"),
+    )?;
+    env.add_template(
+        "create.html.j2",
+        include_str!("../../templates/create.html.j2"),
+    )?;
+    env.add_template(
+        "error.html.j2",
+        include_str!("../../templates/error.html.j2"),
+    )?;
+    env.add_template("faq.html.j2", include_str!("../../templates/faq.html.j2"))?;
+    env.add_template(
+        "fragment.dogears.html.j2",
+        include_str!("../../templates/fragment.dogears.html.j2"),
+    )?;
+    env.add_template(
+        "fragment.tokens.html.j2",
+        include_str!("../../templates/fragment.tokens.html.j2"),
+    )?;
+    env.add_template(
+        "index.html.j2",
+        include_str!("../../templates/index.html.j2"),
+    )?;
+    env.add_template(
+        "install.html.j2",
+        include_str!("../../templates/install.html.j2"),
+    )?;
+    env.add_template(
+        "login.html.j2",
+        include_str!("../../templates/login.html.j2"),
+    )?;
+    env.add_template(
+        "macro.pagination.html.j2",
+        include_str!("../../templates/macro.pagination.html.j2"),
+    )?;
+    env.add_template(
+        "marked.html.j2",
+        include_str!("../../templates/marked.html.j2"),
+    )?;
+    Ok(env)
+}
