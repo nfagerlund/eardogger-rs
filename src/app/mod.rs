919da45d60229036e1727a5c92c91007ae1c6382
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-03-22 11:16:37 -0700

    Add app constructor function... we're getting closer :vibrates:

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,3 +3,26 @@ mod routes;
 mod state;
 mod templates;
 mod web_result;
+
+use self::authentication::session_middleware;
+use self::state::DogState;
+
+use axum::{
+    middleware::from_fn_with_state,
+    routing::{get, post},
+    Router,
+};
+use tower_cookies::CookieManagerLayer;
+
+/// Return a fully-functional eardogger app! The caller is in charge of building
+/// the state, but we DO need it here in order to construct our auth middleware,
+/// since we're using slacker mode instead of writing proper Tower middleware types.
+pub fn eardogger_app(state: DogState) -> Router {
+    let session_auth = from_fn_with_state(state.clone(), session_middleware);
+    Router::new()
+        .route("/", get(routes::root))
+        .route("/login", post(routes::post_login))
+        .layer(session_auth)
+        .layer(CookieManagerLayer::new())
+        .with_state(state)
+}
