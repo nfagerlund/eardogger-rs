699f2298f904c3b3ed4f6376917a4bebd0a84938
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-05-18 12:42:46 -0700

    Split app_tests module apart into helpers / tests

---
+++ src/app/app_tests/mod.rs
@@ -0,0 +1,189 @@
+#![cfg(test)]
+
+mod api;
+mod web;
+
+use axum::body::{to_bytes, Body, Bytes};
+use http::{header, request::Builder, Request, Response, StatusCode};
+use scraper::{Html, Selector};
+use std::sync::Arc;
+use tokio_util::sync::CancellationToken;
+use tokio_util::task::TaskTracker;
+use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
+
+use super::state::*;
+use super::web_result::RawJsonError;
+use super::*;
+use crate::config::DogConfig;
+
+// Right, here's the ground rules for tests in this module. We're taking as
+// axiomatic that DB methods like Dogears::destroy work as advertised, bc
+// they're already type-checked or tested elsewhere. So don't bother w/ cases
+// like wrong user ID. We mostly care about the response formats and status
+// codes here.
+//
+// This file has test helpers. The api and web files have tests.
+
+// SHORTCUTS FOR MAKING THINGS
+
+async fn test_state() -> DogState {
+    let db = crate::db::Db::new_test_db().await;
+    let config = DogConfig::test_config().unwrap();
+    let templates = load_templates().unwrap();
+    let inner = DSInner {
+        db,
+        config,
+        templates,
+        cookie_key: tower_cookies::Key::generate(),
+        task_tracker: TaskTracker::new(),
+        cancel_token: CancellationToken::new(),
+    };
+    Arc::new(inner)
+}
+
+/// Shortcut for request builder w/ method and URI.
+fn new_req(method: impl AsRef<str>, uri: impl AsRef<str>) -> Builder {
+    Request::builder().method(method.as_ref()).uri(uri.as_ref())
+}
+
+// Since https://github.com/tokio-rs/axum/pull/1751, axum routers can't handle
+// type inferrence for the ServiceExt methods because they're no longer generic
+// over the body type. So you have to use the uniform function call syntax, which
+// makes a minor mess... which I am choosing to corral into this thing.
+async fn do_req(app: &mut axum::Router, req: Request<Body>) -> Response<Body> {
+    // gotta love a double-unwrap
+    ServiceExt::<Request<Body>>::ready(app)
+        .await
+        .unwrap()
+        .call(req)
+        .await
+        .unwrap()
+}
+
+/// One-shot CSS selector construction
+fn sel(s: &str) -> Selector {
+    Selector::parse(s).unwrap()
+}
+
+enum Auth<'a> {
+    Token(&'a str),
+    Session(&'a str),
+}
+
+/// A few little extension methods for request::Builder.
+trait TestRequestBuilder {
+    /// Adds bearer auth w/ the provided token cleartext.
+    fn token(self, token: &str) -> Self;
+    /// Adds session auth cookie w/ the provided session ID.
+    fn session(self, session: &str) -> Self;
+    /// Convenience wrapper for reusable test cases: takes either token or session.
+    fn auth(self, auth: Auth) -> Self;
+    /// Sets accept + content-type json.
+    fn json(self) -> Self;
+}
+
+impl TestRequestBuilder for Builder {
+    fn token(self, token: &str) -> Self {
+        self.header(header::AUTHORIZATION, format!("Bearer {}", token))
+    }
+    fn session(self, sessid: &str) -> Self {
+        self.header(header::COOKIE, format!("eardogger.sessid={}", sessid))
+    }
+    fn auth(self, auth: Auth) -> Self {
+        match auth {
+            Auth::Token(t) => self.token(t),
+            Auth::Session(s) => self.session(s),
+        }
+    }
+    fn json(self) -> Self {
+        self.header(header::ACCEPT, "application/json")
+            .header(header::CONTENT_TYPE, "application/json")
+    }
+}
+
+// TRANSFORMING FORMATS
+
+/// Consumes a response to return the body as a Bytes.
+async fn body_bytes(resp: Response<Body>) -> Bytes {
+    to_bytes(resp.into_body(), usize::MAX).await.unwrap()
+}
+
+/// Borrows a Bytes as a &str for quick .contains() checks. Panics on non-utf8.
+fn bytes_str(b: &Bytes) -> &str {
+    std::str::from_utf8(b.as_ref()).unwrap()
+}
+
+/// Consumes a response to return a RawJsonError (or not).
+async fn api_error_body(resp: Response<Body>) -> Result<RawJsonError, serde_json::Error> {
+    let body = body_bytes(resp).await;
+    serde_json::from_slice::<RawJsonError>(&body)
+}
+
+// TESTING OUTCOMES
+
+/// Consumes response. Panics unless it's status 200 and contains the login form.
+async fn assert_login_page(resp: Response<Body>) {
+    assert_page_and_contains(resp, r#"form action="/login""#).await;
+}
+
+/// Consumes response. Panics unless it's status 200 and contains the
+/// specified substring in the body.
+async fn assert_page_and_contains(resp: Response<Body>, substr: &str) {
+    assert_page_and_contains_all(resp, &[substr]).await;
+}
+
+/// Consumes response. Panics unless it's status 200 and contains
+/// ALL of the specified substrings in the body.
+async fn assert_page_and_contains_all(resp: Response<Body>, substrs: &[&str]) {
+    assert_eq!(resp.status(), StatusCode::OK);
+    let body = body_bytes(resp).await;
+    let body_str = bytes_str(&body);
+    for &s in substrs {
+        assert!(body_str.contains(s));
+    }
+}
+
+/// Panics if the response excludes the Access-Control-Allow-Methods
+/// header (which browsers require in order to permit a CORS request).
+fn assert_no_cors(resp: &Response<Body>) {
+    assert!(!resp
+        .headers()
+        .contains_key(header::ACCESS_CONTROL_ALLOW_METHODS));
+}
+
+/// Panics unless the response is a 403 due to insufficient token scope.
+/// This one consumes the response body, so it needs ownership and async.
+async fn assert_api_insufficient_permissions(resp: Response<Body>) {
+    let status = resp.status();
+    let err = api_error_body(resp).await.unwrap();
+    assert_eq!(status, StatusCode::FORBIDDEN);
+    assert!(err.error.contains("permissions"));
+}
+
+/// Does an API request without providing any auth, and panics unless the response
+/// is the expected 401 code and json error object.
+async fn assert_api_auth_required(
+    app: &mut Router,
+    method: &'static str,
+    uri: impl AsRef<str>,
+    body: Option<Body>,
+) {
+    let body = body.unwrap_or(Body::empty());
+    let req = Request::builder()
+        .uri(uri.as_ref())
+        .method(method)
+        .json()
+        .body(body)
+        .unwrap();
+    let resp = do_req(app, req).await;
+    let status = resp.status();
+    let err = api_error_body(resp).await.unwrap();
+    assert_eq!(status, StatusCode::UNAUTHORIZED);
+    assert!(err.error.contains("aren't"));
+}
+
+/// Reports whether page has account link and logout form.
+fn has_logged_in_nav(doc: &Html) -> bool {
+    doc.select(&sel("nav a[href='/account']")).next().is_some()
+        && doc.select(&sel("form#logout")).next().is_some()
+}
---
+++ src/app/app_tests/web.rs
@@ -0,0 +1,186 @@
+use super::app_tests::*;
+
+// /public and /status
+#[tokio::test]
+async fn app_basics_noauth_test() {
+    let state = test_state().await;
+    let mut app = eardogger_app(state.clone());
+
+    // Static file serving is hooked up right
+    {
+        let req = new_req("GET", "/public/style.css")
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_page_and_contains(resp, "--color-background").await;
+    }
+
+    // Status is hooked up right
+    {
+        let req = new_req("GET", "/status").body(Body::empty()).unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::NO_CONTENT);
+    }
+}
+
+#[tokio::test]
+async fn index_test() {
+    let state = test_state().await;
+    let mut app = eardogger_app(state.clone());
+    let user = state.db.test_user("whoever").await.unwrap();
+
+    // No login: serves login page
+    {
+        let req = new_req("GET", "/").body(Body::empty()).unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_login_page(resp).await;
+    }
+    // AuthSession extractor is properly hooked up: token is same as
+    // no login. This is the only time I'll test this, other routes can
+    // just trust the type assurances.
+    {
+        let req = new_req("GET", "/")
+            .token(&user.manage_token)
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_login_page(resp).await;
+    }
+    // Yes login: serves the dogears list.
+    {
+        let req = new_req("GET", "/")
+            .session(&user.session_id)
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        let body = body_bytes(resp).await;
+        let doc = Html::parse_document(bytes_str(&body));
+        // Includes main layout content for a logged-in user:
+        assert!(has_logged_in_nav(&doc));
+        // includes "manual mode" form, for now
+        assert!(doc.select(&sel("form#update-dogear")).next().is_some());
+        // Includes dogears list with all dogears (assumption: test user has 2)
+        assert_eq!(doc.select(&sel("#dogears li")).count(), 2);
+    }
+    // pagination.
+    // Assumption: test user has two dogears.
+    {
+        let req = new_req("GET", "/?size=1")
+            .session(&user.session_id)
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        let body = body_bytes(resp).await;
+        let doc = Html::parse_document(bytes_str(&body));
+        // dogears list present, has #size items
+        assert_eq!(doc.select(&sel("#dogears li")).count(), 1);
+        // pagination controls present: next link, but no prev link
+        assert!(doc
+            .select(&sel(".pagination-link.pagination-previous"))
+            .next()
+            .is_none());
+        let next = doc
+            .select(&sel(".pagination-link.pagination-next"))
+            .next()
+            .expect("must be present");
+        // next link goes to pg 2 w/ specified size
+        assert_eq!(next.attr("href").unwrap(), "/?page=2&size=1");
+        // has correct fragment URL for in-place swap
+        assert_eq!(
+            next.attr("data-fragment-url").unwrap(),
+            "/fragments/dogears?page=2&size=1"
+        );
+    }
+    // pagination part 2
+    {
+        let req = new_req("GET", "/?page=2&size=1")
+            .session(&user.session_id)
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        let body = body_bytes(resp).await;
+        let doc = Html::parse_document(bytes_str(&body));
+        // dogears list present, has #size items
+        assert_eq!(doc.select(&sel("#dogears li")).count(), 1);
+        // pagination controls present: prev link, but no next link
+        assert!(doc
+            .select(&sel(".pagination-link.pagination-next"))
+            .next()
+            .is_none());
+        let prev = doc
+            .select(&sel(".pagination-link.pagination-previous"))
+            .next()
+            .expect("must be present");
+        // prev link goes to pg 1 w/ specified size
+        assert_eq!(prev.attr("href").unwrap(), "/?page=1&size=1");
+        // has correct fragment URL for in-place swap
+        assert_eq!(
+            prev.attr("data-fragment-url").unwrap(),
+            "/fragments/dogears?page=1&size=1"
+        );
+    }
+}
+
+#[tokio::test]
+async fn fragment_dogears_test() {
+    let state = test_state().await;
+    let mut app = eardogger_app(state.clone());
+    let user = state.db.test_user("whoever").await.unwrap();
+
+    // Logged out: 401
+    {
+        let req = new_req("GET", "/fragments/dogears")
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::UNAUTHORIZED);
+    }
+    // Logged in: like the main part of index page.
+    {
+        let req = new_req("GET", "/fragments/dogears")
+            .session(&user.session_id)
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        let body = body_bytes(resp).await;
+        let frag = Html::parse_fragment(bytes_str(&body));
+        // There's NO nav and page frame, it's a fragment.
+        assert!(!has_logged_in_nav(&frag));
+        // Includes dogears list with all dogears (assumption: test user has 2)
+        assert_eq!(frag.select(&sel("#dogears li")).count(), 2);
+    }
+    // Pagination: same as index
+    {
+        // page 2 size 1
+        let req = new_req("GET", "/fragments/dogears?page=2&size=1")
+            .session(&user.session_id)
+            .body(Body::empty())
+            .unwrap();
+        let resp = do_req(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        let body = body_bytes(resp).await;
+        let frag = Html::parse_fragment(bytes_str(&body));
+        // dogears list present, has #size items
+        assert_eq!(frag.select(&sel("#dogears li")).count(), 1);
+        // pagination controls present: prev link, but no next link
+        assert!(frag
+            .select(&sel(".pagination-link.pagination-next"))
+            .next()
+            .is_none());
+        let prev = frag
+            .select(&sel(".pagination-link.pagination-previous"))
+            .next()
+            .expect("must be present");
+        // prev link goes to pg 1 w/ specified size
+        assert_eq!(prev.attr("href").unwrap(), "/?page=1&size=1");
+        // has correct fragment URL for in-place swap
+        assert_eq!(
+            prev.attr("data-fragment-url").unwrap(),
+            "/fragments/dogears?page=1&size=1"
+        );
+    }
+}
